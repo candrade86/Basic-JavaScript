14343bce0b153ec365560503cbeba50f41509a63
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-19 19:59:36 -0500

    Carlos Andrade-Basic-JavaScript

--- src/project-4.js
+++ src/project-4.js
@@ -1,21 +1,26 @@
 const getFirstItem = (collection, cb) => {
   // invoke the callback function and pass the first item from the collection in as an argument
+  cb(collection[0]);
 };
 
 const getLength = (collection, cb) => {
   // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  cb(collection[collection.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // Write a function called sumNums that adds two numbers and passes the result to the callback
+  return cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+  return cb(x * y);
 };
 
 const contains = (collection, item, cb) => {
